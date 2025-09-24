11f755e99b4f9efaf98aa6c29a43c69db2b318c3
efont25 <79472270+efont25@users.noreply.github.com>
2025-09-24 11:01:51 +0100

    Fixes and updates

--- app/(site)/[locale]/[slug]/page.tsx
+++ app/(site)/[locale]/[slug]/page.tsx
@@ -4,23 +4,28 @@ import { PAGE_QUERY } from "@/sanity/lib/queries";
 import NotFound from "@/components/ui/not-found";
 import { cookies } from "next/headers";
 
-export default async function Page(props: any) {
-  const { params } = props as { params: { locale: string; slug: string } };
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ locale: string; slug: string }>;
+}) {
+  const { locale, slug } = await params;
 
-  // read channel from cookie (middleware sets it) and locale from route params
-  const cookieStore = cookies();
-  const channel = (await cookieStore).get("channel")?.value || "1spWeb";
-  const language = params.locale || "en";
-  const slug = params.slug;
+  const cookieStore = await cookies();
+  const channel = cookieStore.get("channel")?.value || "1spWeb";
+  const language = locale || "en";
 
   const { data: page } = await sanityFetch({
     query: PAGE_QUERY,
     params: { slug, channel, language },
   });
-
   return (
-    <main className="pt-20 md:pt-24 lg:pt-28 min-h-screen">
-      {page?.content ? <PageBuilder content={page.content} /> : <NotFound />}
-    </main>
+    <div className=" min-h-screen">
+      {Array.isArray(page?.content1sp) ? (
+        <PageBuilder content={page.content1sp} />
+      ) : (
+        <NotFound />
+      )}
+    </div>
   );
 }
