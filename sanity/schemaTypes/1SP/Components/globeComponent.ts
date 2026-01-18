feb40d3ae84bf721cc8fddaa0a3514f6b836c4b4
mainframeai <martin@definition.studio>
2026-01-18 20:17:54 +0100

    pagebuilder navigation sidebar omission option 2

--- sanity/schemaTypes/1SP/Components/globeComponent.ts
+++ sanity/schemaTypes/1SP/Components/globeComponent.ts
@@ -20,6 +20,14 @@ export default defineType({
             group: 'navigation',
         }),
         defineField({
+            name: 'hideFromNav',
+            title: 'Hide from Navigation',
+            type: 'boolean',
+            description: 'If enabled, this section will not appear in the vertical navigation minimap.',
+            initialValue: false,
+            group: 'navigation',
+        }),
+        defineField({
             name: 'sectionTitle',
             title: 'Section Title',
             type: 'string',
