d3e269bd2d1967417c6ac606227346e1980803fa
mainframeai <martin@definition.studio>
2026-03-23 19:36:15 +0100

    fix: simplify browserToken handling in live.ts by using a static token

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -1,5 +1,4 @@
 import { defineLive } from 'next-sanity/live'
-import { draftMode } from 'next/headers'
 import { client } from './client'
 
 const token = process.env.SANITY_VIEWER_TOKEN
@@ -7,8 +6,5 @@ const token = process.env.SANITY_VIEWER_TOKEN
 export const { sanityFetch, SanityLive } = defineLive({
   client,
   serverToken: token,
-  browserToken: async () => {
-    const { isEnabled } = await draftMode()
-    return isEnabled ? token : undefined
-  },
+  browserToken: token,
 })
