55bcb80b329fb620d0c51a3a94a07c869443b2b1
mainframeai <martin@definition.studio>
2026-03-24 19:09:09 +0100

    fix: adjust width and positioning for improved layout in CaseGalleryComponent

--- components/data/data-CaseGallery.tsx
+++ components/data/data-CaseGallery.tsx
@@ -108,7 +108,7 @@ export default function CaseGalleryComponent({
               exit={{ opacity: 0, transition: { duration: 0.05 } }}
               transition={{ type: "spring", visualDuration: 0.3, bounce: 0.2 }}
               ref={ref}
-              className="w-full max-w-[960px] min-h-[70vh] max-h-[100vh] rounded-xl grid bg-neutral-900 dark:bg-neutral-900 shadow-2xl overflow-hidden"
+              className="w-[95vw] md:w-full max-w-[960px] min-h-[70vh] max-h-[100vh] rounded-xl grid bg-neutral-900 dark:bg-neutral-900 shadow-2xl overflow-hidden"
             >
               <motion.div
                 className="[grid-area:1/1] w-full sm:rounded-t-xl opacity-80  min-h-[70vh] max-h-[100vh] mediabackground"
@@ -142,7 +142,7 @@ export default function CaseGalleryComponent({
                   initial={{ opacity: 1 }}
                   animate={{ opacity: 1 }}
                   exit={{ opacity: 1, transition: { duration: 0.05 } }}
-                  className="flex absolute top:24 md:top-10  right-4 lg items-center hover:cursor-pointer overflow-hidden justify-around rounded-full h-6 w-6 z-100"
+                  className="flex absolute top-4  md:top-10  right-4 lg items-center hover:cursor-pointer overflow-hidden justify-around rounded-full h-6 w-6 z-100"
                   onClick={() => setActive(null)}
                   aria-label="Close"
                 >
