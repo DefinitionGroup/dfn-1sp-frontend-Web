7c253bf781e10b20954202b3e0fd3eade98ba91f
mainframeai <martin@definition.studio>
2026-04-30 09:47:26 +0200

    feat: comment out CookieDeclaration component for improved code clarity

--- app/(site)/[locale]/[slug]/page.tsx
+++ app/(site)/[locale]/[slug]/page.tsx
@@ -224,7 +224,7 @@ export default async function Page({
               language={language}
               deferAfter={2}
             />
-            {slug === "data-protection" && <CookieDeclaration />}
+            {/* {slug === "data-protection" && <CookieDeclaration />} */}
           </>
         ) : (
           <NotFound />
